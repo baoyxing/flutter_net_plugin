37e150e752e9433f0a84747740f7dd72fd5ef443
baoyx <baoyuexing@grandhelmsman.com>
2020-10-14 14:48:31 +0800

    plugin init

---
+++ lib/data_helper.dart
@@ -0,0 +1,26 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:dio/dio.dart';
+class DataHelper {
+  static SplayTreeMap getBaseMap() {
+    var map = new SplayTreeMap<String,dynamic>();
+    map["time"] = new DateTime.now().microsecondsSinceEpoch.toString();
+    return map;
+  }
+
+  static encryptParams(SplayTreeMap<String, dynamic> map,String signKey) {
+    var buffer = StringBuffer();
+    buffer.write(signKey);
+    map.forEach((key, value) {
+      buffer.write(key + "=" + value+"&");
+    });
+    buffer.write(signKey);
+    var sign = sha512.convert(utf8.encode(buffer.toString())).toString();
+    map["sign"] = sign;
+    FormData formData = FormData.fromMap(map);
+    return formData;
+  }
+
+
+}
